8fa0ac46367ded4eb6bed77fc069de49da660dbc
lekan <bidowuolalekan@gmail.com>
2022-09-09 15:08:02 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - to print if the number is greater than 0,
+ * less than 0,
+ * or equal to 0
+ * Return: always success
+ */
+
+int main(void)
+
+{
+
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+	print("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+	print("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+	print("%d is negative\n", n);
+	}
+	return (0);
+
+}

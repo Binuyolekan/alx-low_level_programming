8ec5120ed1da3d60c67cd21c50618d6e1d7f9102
lekan <bidowuolalekan@gmail.com>
2022-09-11 13:35:14 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Return:0 Always Success
+*/
+
+int main(void)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+	printf("%d", i);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}

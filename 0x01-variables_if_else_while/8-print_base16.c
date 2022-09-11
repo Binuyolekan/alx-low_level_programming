dedea8b97491dc55032c07e1ca1d96dced1857f3
lekan <bidowuolalekan@gmail.com>
2022-09-11 14:32:09 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Return:0 Always Success
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+	char j;
+
+
+
+	for (i = 48; i < 58; i++)
+
+	{
+
+
+
+	putchar(i);
+
+	}
+
+	for (j = 'a'; j <= 'f'; j++)
+
+	{
+
+	putchar(j);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

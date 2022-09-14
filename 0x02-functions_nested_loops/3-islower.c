753920e5b4b8d3d2997a9653d23b146f56a39455
lekan <bidowuolalekan@gmail.com>
2022-09-13 18:41:26 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - function to check for a-z
+ * @c: functin
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+	{
+
+	return (1);
+
+	}
+
+	else
+
+	return (0);
+}

d94d3d1896aa4d20f526ac3765600e4578767093
lekan <bidowuolalekan@gmail.com>
2022-09-13 19:22:02 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function declaration
+ * @n: instruction
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n < o)
+	{
+	_putchar('-');
+	return (-1);
+	{
+	else
+	}
+	_putchar('0');
+	return (0);
+	}
+}
+

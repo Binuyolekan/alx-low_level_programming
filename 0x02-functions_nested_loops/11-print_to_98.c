6bbf6cec6344b5a302568c040586cf02ebf5a52b
lekan <bidowuolalekan@gmail.com>
2022-09-13 21:59:48 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * print_to_98 - print all natural num
- * @n: the number to begin counting it
+ * @n: the number to begin counting at.
  */
 
 void print_to_98(int n)
@@ -11,14 +11,17 @@ void print_to_98(int n)
 
 	if (n >= 98)
 	{
-		while (n > 98)
-			printf("%d, ", n--);
-		printf("%d\n", n);
+	while (n > 98)
+
+	printf("%d, ", n--);
+	printf("%d\n", n);
 	}
 	else
 	{
+
 	while (n < 98)
-		 printf("%d, ", n++);
-		printf("%d\n", n);
+
+	printf("%d, ", n++);
+	printf("%d\n", n);
 	}
 }

16741953043ba1a58b58fd6369bf7a006b23155d
lekan <bidowuolalekan@gmail.com>
2022-09-13 19:34:42 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,13 +13,13 @@ int print_sign(int n)
 	_putchar('+');
 	return (1);
 	}
-	else if (n < o)
+	else if (n < 0)
 	{
 	_putchar('-');
 	return (-1);
-	{
-	else
 	}
+	else
+	{
 	_putchar('0');
 	return (0);
 	}

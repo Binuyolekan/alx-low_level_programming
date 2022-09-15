1d3ad7142a9e43be10e399e3bd57c8eb29ed759a
lekan <bidowuolalekan@gmail.com>
2022-09-15 03:42:52 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,12 +3,9 @@
 
 
 /**
- *
- * * main - causes an infinite loop
- *
- * * Return: 0
- *
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
 
 
@@ -16,32 +13,28 @@ int main(void)
 
 {
 
-	        int i;
+	int i;
 
 
 
-		        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
 
 
-			        i = 0;
+	i = 0;
 
 
-/**
-				        while (i < 10)
+	/**
+	 * while (i < 10)
+	 * {
+	 *
+	 * putchar(i);
+	 * }
+	 */
 
-					        {
-
-								                putchar(i);
-
-										        }
-*/
-
-
-					        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 
-
-						        return (0);
+	return (0);
 
 }

49f79bfea49095d8af8ff9a757caa0bb49f7b52b
lekan <bidowuolalekan@gmail.com>
2022-09-19 20:26:17 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - string length function
+ * @s: parameter
+ * Return: int 
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0');
+	{
+	count++;
+	}
+	return (count);
+}
